30494f9201ea6179ec18a0578989c3eb4eb35e70
Willem Victor <wimpievictor@gmail.com>
2021-01-09 12:01:39 +0200

    Benchmark tweaks

--- rlox-gc/benches/bench_alloc.rs
+++ rlox-gc/benches/bench_alloc.rs
@@ -1,8 +1,11 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use rlox_gc::{Collector};
+use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
+use rlox_gc::Collector;
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("alloc 8Byte", |b| {
+    let mut group = c.benchmark_group("alloc");
+
+    group.throughput(Throughput::Bytes(8));
+    group.bench_function(BenchmarkId::from_parameter(8), |b| {
         let mut collector = Collector::new();
 
         b.iter(|| collector.alloc(black_box([0u8; 8])));
@@ -10,7 +13,8 @@ fn criterion_benchmark(c: &mut Criterion) {
         collector.collect()
     });
 
-    c.bench_function("alloc 24Byte", |b| {
+    group.throughput(Throughput::Bytes(24));
+    group.bench_function(BenchmarkId::from_parameter(24), |b| {
         let mut collector = Collector::new();
 
         b.iter(|| collector.alloc(black_box([0u8; 24])));
@@ -18,13 +22,16 @@ fn criterion_benchmark(c: &mut Criterion) {
         collector.collect()
     });
 
-    c.bench_function("alloc 32Byte", |b| {
+    group.throughput(Throughput::Bytes(32));
+    group.bench_function(BenchmarkId::from_parameter(32), |b| {
         let mut collector = Collector::new();
 
         b.iter(|| collector.alloc(black_box([0u8; 32])));
 
         collector.collect()
     });
+
+    group.finish();
 }
 
 criterion_group!(benches, criterion_benchmark);
--- rlox-gc/benches/bench_collect.rs
+++ rlox-gc/benches/bench_collect.rs
@@ -1,36 +1,45 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use rlox_gc::{Collector};
+use criterion::{black_box, criterion_group, criterion_main, Criterion, Throughput};
+use rlox_gc::Collector;
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("collect 8Byte * 1000000", |b| {
-        let mut collector = Collector::new();
-        let mut objs = vec![];
-        for _ in 0..1000000 {
-            objs.push(collector.alloc([0u8; 8]));
-        }
+    let mut group = c.benchmark_group("alloc");
 
-        b.iter(|| collector.collect())
-    });
+    for count in &[1000, 10000, 1000000] {
+        group.throughput(Throughput::Bytes(count * 8));
+        group.bench_function(format!("8/{}", count), |b| {
+            let mut collector = Collector::new();
+            let mut objs = vec![];
+            for _ in 0..1000000 {
+                objs.push(collector.alloc(black_box([0u8; 8])));
+            }
 
-    c.bench_function("collect 24Byte * 1000000", |b| {
-        let mut collector = Collector::new();
-        let mut objs = vec![];
-        for _ in 0..1000000 {
-            objs.push(collector.alloc([0u8; 24]));
-        }
+            b.iter(|| collector.collect())
+        });
 
-        b.iter(|| collector.collect())
-    });
+        group.throughput(Throughput::Bytes(count * 24));
+        group.bench_function(format!("24/{}", count), |b| {
+            let mut collector = Collector::new();
+            let mut objs = vec![];
+            for _ in 0..1000000 {
+                objs.push(collector.alloc(black_box([0u8; 24])));
+            }
 
-    c.bench_function("collect 32Byte * 1000000", |b| {
-        let mut collector = Collector::new();
-        let mut objs = vec![];
-        for _ in 0..1000000 {
-            objs.push(collector.alloc([0u8; 32]));
-        }
+            b.iter(|| collector.collect())
+        });
 
-        b.iter(|| collector.collect())
-    });
+        group.throughput(Throughput::Bytes(count * 32));
+        group.bench_function(format!("32/{}", count), |b| {
+            let mut collector = Collector::new();
+            let mut objs = vec![];
+            for _ in 0..1000000 {
+                objs.push(collector.alloc(black_box([0u8; 32])));
+            }
+
+            b.iter(|| collector.collect())
+        });
+    }
+
+    group.finish();
 }
 
 criterion_group!(benches, criterion_benchmark);
